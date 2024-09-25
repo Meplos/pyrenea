efaf86644a4b85e69e194287f2bd9c6191d8c906
Meplos <alexxd64150@gmail.com>
2024-09-25 23:38:41 +0200

    release: 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aerard.pyrenea"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "OSSAU - 1.1.0"
+        versionCode = 4
+        versionName = "OSSAU - 1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
